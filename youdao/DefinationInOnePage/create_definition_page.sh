9f85fb1c351812103dba7f2127eb6aef4f36bcc5
bow.minus <bow.minus@gmail.com>
2013-02-05 19:28:12 -0700

    mobile version

--- youdao/DefinationInOnePage/create_definition_page.sh
+++ youdao/DefinationInOnePage/create_definition_page.sh
@@ -1,14 +1,53 @@
 #!bash
 
-words=`cat $1`
-for word in $words
-do
-  if [ ! -f cache/${word}.html ]
+getBaseFileName()
+{
+	filename=$(basename "$1")
+	filename="${filename%.*}"
+}
+
+function createOneFile()
+{
+	if [ ! -f $1 ]
 	then
-		curl "http://dict.youdao.com/search?le=eng&q=${word}&keyfrom=dict.index" > cache/${word}.html
+		echo "$1 is not a file"
+		anywait
+		exit
 	fi
-done
 
-cd cache
+	words=`cat $1`
 
-../GeneratePage.exe $words > ../definition.html
+	for word in $words
+	do
+		if [ ! -f cache/${word}.html ]
+		then
+			curl "http://dict.youdao.com/search?le=eng&q=${word}&keyfrom=dict.index" > cache/${word}.html
+		fi
+	done
+
+	cd cache
+
+	
+	getBaseFileName "../${1}"
+	../bin/GeneratePage.exe $words > $2/${filename}.html
+	cd ..
+}
+
+echo "Input the file or folder name"
+read path
+
+if [ -f $path ] 
+then
+	createOneFile $path ..
+elif [ -d $path ] 
+then
+	for fileitem in `ls $path`
+	do
+		echo $path/$fileitem
+		createOneFile $path/$fileitem ../output
+	done
+else
+	echo -e "\n###Error: $path is not a file or folder.\nPress Enter to exit."
+	read
+fi
+
