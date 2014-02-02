6fce1556f8a62d58fafb236a9ced3881400aab0e
cong <ma.cong@outlook.com>
2014-02-01 22:09:19 -0700

    3星也过滤

--- lemmatizer/filter.sh
+++ lemmatizer/filter.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
 ./scripts/lemmatizer.py $(pwd)/$1 | tr '[:upper:]' '[:lower:]' | sort -u > uni
-fgrep -xvf words/word45 uni > result
-fgrep -xf words/word3 result > newwords3
-fgrep -xf words/word2 result > newwords2
-fgrep -xf words/word1 result > newwords1
-rm uni
-rm result
+fgrep -xvf words/word45 uni > without45
+fgrep -xvf words/word3 without45 > without345
+fgrep -xf words/word2 without345 > newwords2
+fgrep -xf words/word1 without345 > newwords1
+rm uni without45 without345
